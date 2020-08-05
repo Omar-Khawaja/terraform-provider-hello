7a486262f1a679444b4f94ea8fe0b7558eb297e0
Omar Khawaja <khawaja910@gmail.com>
2020-08-05 17:11:41 -0400

    update build and install scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-go build -o ../terraform-provider-hello_$(git describe --tags) ..
+go build -o ../terraform-provider-hello_$(git describe --tags --abbrev=0) ..
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-go build -o terraform-provider-hello_$(git describe --tags) ..
-mv terraform-provider-hello_$(git describe --tags) ~/.terraform.d/plugins/
+go build -o terraform-provider-hello_$(git describe --tags --abbrev=0) ..
+mv terraform-provider-hello_$(git describe --tags --abbrev=0) ~/.terraform.d/plugins/
